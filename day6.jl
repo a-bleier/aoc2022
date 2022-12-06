78848a6a801494cd262160af2fd1ceb09bb2eb40
Adrian <bleier.adrian@gmx.de>
2022-12-06 21:20:47 +0100

    speed up day 6

--- day6.jl
+++ day6.jl
@@ -1,18 +1,37 @@
 function solve(input)
-	res1 ::Int, res2 ::Int = 0, 0
-	for i in 1:length(input[1])-3
-		if length(Set{Char}(input[1][i:i+3])) == 4
-			res1 = i + 3
-			break
-		end
-	end
-
-	for i in 1:length(input[1])-13
-		if length(Set{Char}(input[1][i:i+13])) == 14
-			res2 = i + 13
-			break
-		end
-	end
-
+	res1 = find_offset(4, input[1])
+	res2 = find_offset(14, input[1])
 	return res1, res2
+end
+
+function find_offset(marker_length ::Int, input ::String) ::Int
+	counter = Dict{Char, Int}(zip("abcdefghijklmnopqrstuvwxyz", zeros(26)))
+	single_letters ::Int = 0
+	res1 ::Int, res2 ::Int = 0, 0
+	for (i,current) in enumerate(input)
+
+		current ::Char = input[i]
+
+		counter[current] += 1
+		if counter[current] == 1
+			single_letters += 1
+		elseif counter[current] == 2
+			single_letters -= 1
+		end
+		
+		if i > marker_length
+			counter[input[i-marker_length]] -= 1
+			if counter[input[i-marker_length]] == 1
+				single_letters += 1
+			elseif counter[input[i-marker_length]] == 0 
+				single_letters -= 1
+			end
+		end
+
+		if single_letters == marker_length
+			return i
+			break
+		end
+	end
+	return 0
 end
